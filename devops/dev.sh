5ef0b4a5f4b1a952e65f9459df6e121b97ce8e22
alexdaima <61457613+alexdaima@users.noreply.github.com>
2023-12-10 08:59:10 +0800

    Rename backend to api

--- devops/dev.sh
+++ devops/dev.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 KUBEPATH=$SCRIPT_DIR/k8s/localhost
 
 
-BACKEND_CODE_PATH=$(cd $SCRIPT_DIR/../backend && pwd)
+API_CODE_PATH=$(cd $SCRIPT_DIR/../api && pwd)
 cp $KUBEPATH/api.yaml $KUBEPATH/api.modified.yaml 
-sed -i -e 's|<BACKEND_CODE_PATH>|'$BACKEND_CODE_PATH'|' $KUBEPATH/api.modified.yaml
+sed -i -e 's|<API_CODE_PATH>|'$API_CODE_PATH'|' $KUBEPATH/api.modified.yaml
 [ -f "$KUBEPATH/api.modified.yaml-e" ] && rm -fv "$KUBEPATH/api.modified.yaml-e"
 echo "Generated k8s/localhost/api.modified.yaml"
 
